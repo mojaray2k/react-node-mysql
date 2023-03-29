cadd5f9f95ee6ec21c2a414719058a0a0a719e18
Amen Moja Ra <mojaray2k@gmail.com>
2023-03-29 17:33:20 -0400

    Initial database connection setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const mysql = require("mysql");
 
 const app = express();
+const db = mysql.createConnection({
+    user: "root",
+    port: 3306,
+    host: 'localhost',
+    password: 'password',
+    database: 'ReactNodeCrudDatabase'
+})
 // Init Middleware
 // app.use(express.static(__dirname + "/public"));
 app.use(morgan("dev"));
@@ -9,7 +17,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    // const movie_name = "The Five Heartbeats";
+    // const movie_review = "This movie is on all my favorite movie lists.";
+
+    // db.query(
+    //     'INSERT INTO movie_reviews (movie_name, movie_review) VALUES (?, ?)', [movie_name, movie_review],
+    //     (err, result) => {
+    //         if (err) {
+    //             console.log("[mysql error]",err);
+    //         }
+
+            res.send("Hello World")
+    //     }
+    // );
+    
 })
 
 app.listen(3001, (err) => {
